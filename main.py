84a527fc159e3adbe27536afb9347be9985c88fc
De1cans <15901@coderacademy.edu.au>
2024-09-28 19:52:40 +1000

    added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from classes.hotel import Hotel
-from functions.hotel_functions import add_floor, list_floors
+from functions.admin_functions import add_floor, list_floors
 
 print("\n------------------------")
 print("Welcome To The My Hotel!")
@@ -21,6 +21,19 @@ choice = ""
 
 hotel = Hotel("hotell")
 
+def admin_menu():
+    print("Enter 1 to add floors")
+    print("Enter 2 to list floors")
+    print("Enter 3 to add rooms")
+    print("Enter 4 to list rooms")
+    print("Enter 5 to return to hotel menu")
+
+    admin_choice = input("Please select a number from the list above: ")
+
+    return admin_choice
+
+admin_choice = ""
+
 while choice != "6":
     choice = hotel_menu()
 
@@ -33,9 +46,25 @@ while choice != "6":
     elif choice == "4":
         print("You've entered 4")
     elif choice == "5":
-        print("You've entered 5")
+        admin_menu()
+    elif choice == "6":
+        print("Exiting")
     else:
         print("Invalid choice")
+
+while admin_choice != "5":
+    admin_choice = admin_menu()
+
+    if admin_choice == "1":
+        list_floors(hotel)
+    elif admin_choice == "2":
+        add_floors(hotel)
+    elif admin_choice == "3":
+        print("Admin choice 3")
+    elif admin_choice == "4":
+        print("Admin choice 4")
+    elif admin_choice == "5":
+        hotel_menu()
 
 print("\n--------------------------------")
 print("Thank you for visiting the Hotel")
